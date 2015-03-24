d9be0c26f1082af035e17862d1cf2eca9f89289b
Andrew Fishberg <fishberg.github@gmail.com>
2015-03-24 10:17:32 -0700

    added output file to GoodreadsFetch

--- java/workspace/GoodreadsFetch/src/GR.java
+++ java/workspace/GoodreadsFetch/src/GR.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -22,12 +23,14 @@ public class GR {
 		scan.close();
 		
 		// "https://www.goodreads.com/search.xml?key=YOUR_KEY&q=Ender%27s+Game"
-		url = new URL("https://www.goodreads.com/search.xml?key=" + key + "&q=Ender%27s+Game");
+		url = new URL("https://www.goodreads.com/search/index.xml?key=" + key + "&q=Ender%27s+Game");
 		is = url.openStream(); // throws an IOException
 		br = new BufferedReader(new InputStreamReader(is));
 
+		PrintWriter pw = new PrintWriter(new File("out.xml"));
 		while ((line = br.readLine()) != null) {
 			System.out.println(line);
+			pw.println(line);
 		}
 
 		is.close();
