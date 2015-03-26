d5d22439aeaee8ced4832c0386eb80db92d10c76
Hannah Young <hyoung@hmc.edu>
2015-03-26 12:15:37 -0700

    fixed the status view and started work on dialog

--- android/BookExchange/app/src/main/java/com/example/jarthur/bookexchange/Status.java
+++ android/BookExchange/app/src/main/java/com/example/jarthur/bookexchange/Status.java
@@ -1,12 +1,24 @@
 package com.example.jarthur.bookexchange;
 
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class Status extends ActionBarActivity {
+
+    // controls whether we see request, borrow, or loan
+    int type = 0;
+    final int REQUEST = 0;
+    final int BORROW = 1;
+    final int LOAN = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +28,42 @@ public class Status extends ActionBarActivity {
         // Make logo show up in action bar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        // TODO Call database and set type
+
+        // Sets visibilities correctly based on type
+        // Pages can show book, otherPerson, contactInfo, dueDate, finishButton
+        switch(type){
+            // Does not show dueDate
+            case REQUEST:
+                View b = findViewById(R.id.dueDate);
+                b.setVisibility(View.GONE);
+                break;
+
+            // Does not show finishButton
+            case BORROW:
+                View c = findViewById(R.id.finishButton);
+                c.setVisibility(View.GONE);
+                break;
+
+            // Shows everything
+            case LOAN:
+                break;
+
+            // Shows everything
+            default:
+                break;
+        }
+
+
+        DialogFragment dialog = new YesNoDialog();
+        Bundle args = new Bundle();
+        args.putString("title", "T");
+        args.putString("message", "M");
+        dialog.setArguments(args);
+        //dialog.setTargetFragment(this, YES_NO_CALL);
+        dialog.show(getFragmentManager(), "tag");
+
     }
 
 
@@ -40,4 +88,7 @@ public class Status extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
