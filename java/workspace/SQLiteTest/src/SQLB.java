f934bbe8e51d4a5d64993bf4ea8c32428f0d08e6
Hannah Rose <hrose@hmc.edu>
2015-04-03 14:24:41 -0700

    Finished writing sql command generation, still need to add a test class.

--- java/workspace/SQLiteTest/src/SQLB.java
+++ java/workspace/SQLiteTest/src/SQLB.java
@@ -2,29 +2,54 @@
 
 // SQL Builder
 public abstract class SQLB { //TODO Sanitize
-	static String getSpecificExchange(String exchangeID) {
-		return "";
+
+	// Does not guarantee that user is allowed to view the exchange.
+	static String getExchange(Integer exchangeId) {
+		return "select * from exchanges where exchange_id = " + exchangeId;
 	}
 	
-//	static String getPublicExchange(String a) {
-//		return "";
-//	}
-//	
-//	static String getPrivateExchange() {
-//		return "";
-//	}
-//	
-	static String createExchangeLoan(Integer loaner, Integer book, String title) {
-		String command = "insert into exchanges (loaner_id, book_id, book_title, create_date, status) values (%d, %d, '%s', datetime('now'), 0);";
-		return String.format(command, loaner, book, title);
+	static String getPublicExchanges() {
+		return "select * from exchanges where status = 'INITIAL' or status = 'RESPONSE';";
 	}
 	
-//	static String createExchangeBorrow() {
-//		return "";
-//	}
+	static String getPrivateExchanges(Integer userId) {
+		return "select * from exchanges where loaner_id = " + userId + " or borrower_id = " + userId + ";";
+	}
 	
+	static String createExchangeLoan(Integer loanerId, Integer bookId, String title) {
+		String command = "insert into exchanges (loaner_id, book_id, book_title, create_date, status) values (%d, %d, '%s', datetime('now'), 'INITIAL');";
+		return String.format(command, loanerId, bookId, title);
+	}
+	
+	static String createExchangeBorrow(Integer borrowId, Integer bookId, String title) {
+		String command = "insert into exchanges (borrower_id, book_id, book_title, create_date, status) values (%d, %d, '%s', datetime('now'), 'INITIAL');";
+		return String.format(command, borrowId, bookId, title);
+	}
+
+	static String updateBookStatus(Status newStatus, Integer id) {
+		return "update exchanges set status = '" + newStatus + "' where exchange_id = " + id + ";";
+	}
+	
+	static String insertBook(Integer bookId, String title, String author, String isbn, Integer pubYear, Integer origPubYear,
+								String imageUrl, String smallImageUrl) {
+		return "insert into books values ("
+				+ bookId + ",'" + title + "','" + author + "','" + isbn + "'," + pubYear + "," + 
+				origPubYear + ",'" + imageUrl + "','" + smallImageUrl + "'," + " datetime('now'));";
+	}
+		
 	static String getOldestBook() {
-		return "select min(add_date) from books;\n"
-		+ "select book_id from books where add_date = minDate;";
+		return "select book_id from books where add_date = (select min(add_date) from books);";
+	}
+	
+	static String findBookFromISBN(String isbn) {
+		return "select book_id from books where isbn = " + isbn;
+	}
+	
+	static String isBookActive(Integer bookId) {
+		return "select * from exchanges where book_id = " + bookId + " and status != 'COMPLETED';";
+	}	
+	
+	static String deleteBook(Integer bookId) {
+		return "delete * from books where book_id = bookId;";
 	}
 }
--- java/workspace/SQLiteTest/src/SQLiteDriver.java
+++ java/workspace/SQLiteTest/src/SQLiteDriver.java
@@ -12,6 +12,13 @@ public class SQLiteDriver {
 		
 		String comm = SQLB.createExchangeLoan(100, 200, "C");
 		System.out.println(comm);
+		
+		update(comm);
+		
+		comm = SQLB.insertBook(21413662, "What If?", "Randall Munroe", "0544272994", 2014, 2014,
+				"https://d.gr-assets.com/books/1394648139m/21413662.jpg", "https://d.gr-assets.com/books/1394648139m/21413662.jpg");
+		System.out.println(comm);
+		
 		update(comm);
 		
 		//select();
